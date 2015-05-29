226843b07d7335508762b27187d8ad587456d0a6
y.m.farag@gmail.com <y.m.farag@gmail.com@f45b561b-ed20-3bbc-d8f9-8bb4e43af963>
2015-05-29 16:46:48 +0000

    organize the layout for dataset exploring ad single/multiple selection fix the bug in the bar charts add multiple selection support for the chart selections

---
+++ trunk/CSF-PR-V4/src/main/webapp/VAADIN/widgetsets/probe.com.AppWidgetSet/deferredjs/FF2885162060BD2026C18DE067E75332/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.probe_com_AppWidgetSet.runAsyncCallback2('Hab(1531,1,jMd);_.tc=function k9b(){jXb((!cXb&&(cXb=new oXb),cXb),this.a.d)};QFd(Qh)(2);\n//# sourceURL=probe.com.AppWidgetSet-2.js\n')
