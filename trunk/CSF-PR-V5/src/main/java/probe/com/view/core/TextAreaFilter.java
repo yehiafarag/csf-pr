ae6a40a36702396b3eca786d29557ab27872a961
y.m.farag@gmail.com <y.m.farag@gmail.com@f45b561b-ed20-3bbc-d8f9-8bb4e43af963>
2015-06-24 20:33:11 +0000

    add csf-pr v5

---
+++ trunk/CSF-PR-V5/src/main/java/probe/com/view/core/TextAreaFilter.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probe.com.view.core;
+
+import com.vaadin.data.Property;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.themes.Reindeer;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import probe.com.view.components.SearchingFiltersControl;
+
+/**
+ *
+ * @author Yehia Farag
+ */
+public class TextAreaFilter extends TextArea implements Property.ValueChangeListener, Serializable {
+
+    private final ClosableFilterLabel filterBtn;
+    private final SearchingFiltersControl control;
+    private final int filterId;    ;
+    private final String filterTitle;
+
+    public ClosableFilterLabel getFilterBtn() {
+        return filterBtn;
+    }
+
+    public TextAreaFilter(SearchingFiltersControl controller, int filterId, String filterTitle, String defaultText) {
+        this.control = controller;
+        this.filterId = filterId;
+        this.filterTitle = filterTitle;
+        this.setWidth("437px");
+        this.setImmediate(true);
+        this.setStyleName(Reindeer.TEXTFIELD_SMALL);
+        this.setInputPrompt(defaultText);
+        this.addValueChangeListener(TextAreaFilter.this);
+
+//        filterBtn.getCloseBtn().addClickListener(this);
+//        okBtn.addClickListener(new Button.ClickListener() {
+//            @Override
+//            public void buttonClick(Button.ClickEvent event) {
+//                
+//        if(textField.getValue()!= null && !textField.getValue().trim().equalsIgnoreCase(""))
+////        {
+//        
+//            filterBtn.setCaption(textField.getValue().trim());
+//            control.addFilter(filterBtn);
+////        
+//        }}
+//        }); 
+        filterBtn = new ClosableFilterLabel(filterTitle, "", filterId, true);
+
+    }
+    private final Set<String> keywordsSet = new HashSet<String>();
+
+    @Override
+    public void valueChange(Property.ValueChangeEvent event) {
+        keywordsSet.clear();
+        for (String str : this.getValue().split("\n")) {
+            keywordsSet.add(str.toUpperCase());
+        }
+        control.removeFilter(filterBtn.getCaption());
+        if (this.getValue() != null && !this.getValue().trim().equalsIgnoreCase("")) {
+            String key = keywordsSet.toString().trim();
+            key = key.substring(1, key.length()-1);
+            filterBtn.setValue(key);
+            control.addFilter(filterBtn);
+//        
+        }
+    }
+
+}
---
+++ trunk/CSF-PR-V5/src/main/java/probe/com/view/quantdatasetsoverview/DatasetsOverviewLayout.java
@@ -0,0 +1,108 @@
+package probe.com.view.quantdatasetsoverview;
+
+import com.vaadin.event.LayoutEvents;
+import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.VerticalLayout;
+import probe.com.selectionmanager.DatasetExploringSelectionManagerRes;
+import probe.com.handlers.MainHandler;
+import probe.com.selectionmanager.FilterUtility;
+import probe.com.view.quantdatasetsoverview.heatmap.DatasetExploringHeatMapFilters;
+import probe.com.view.components.ExploreDatasetsTableLayout;
+import probe.com.view.core.HideOnClickLayout;
+import probe.com.view.quantdatasetsoverview.popupinteractivefilter.PopupInteractiveFilterComponent;
+import probe.com.view.quantdatasetsoverview.quantcomparisontable.UpdatedQuantProteinsComparisonTable;
+
+/**
+ * This is the studies layout include publication heatmapFiltere and publication table
+ *
+ * @author Yehia Farag
+ */
+public class DatasetsOverviewLayout extends VerticalLayout {
+
+    private final PopupInteractiveFilterComponent filtersLayout;
+//     private final DatasetsExplorerTreeLayout studiesExplorerTreeLayout;
+    private final FilterUtility filterUtility;
+    private final DatasetExploringSelectionManagerRes exploringFiltersManager;
+    private final  ProteinsLayout proteinsLayout;
+    private final   UpdatedQuantProteinsComparisonTable  compTableLayout;
+
+    public DatasetsOverviewLayout(MainHandler handler) {
+
+        filterUtility = new FilterUtility(handler);
+        exploringFiltersManager = new DatasetExploringSelectionManagerRes(filterUtility.getQuantDatasetArr(), filterUtility.getActiveFilters());//,filterUtility.getFullFilterList()
+
+        this.setMargin(true);
+        this.setSpacing(true);
+        this.setWidth("100%");
+        this.setHeightUndefined();
+        
+        //init level 1 heatmapFiltere    
+        filtersLayout = new PopupInteractiveFilterComponent(exploringFiltersManager);
+//        HorizontalLayout comparisonLayout = new HorizontalLayout();
+//        comparisonLayout.setWidth("100%");
+//        this.addComponent(comparisonLayout);
+        
+        
+        compTableLayout = new UpdatedQuantProteinsComparisonTable(exploringFiltersManager, handler);        
+        DatasetExploringHeatMapFilters heatmapFilter = new DatasetExploringHeatMapFilters(exploringFiltersManager,filtersLayout,compTableLayout);
+        heatmapFilter.setWidth("100%");
+        heatmapFilter.setMargin(new MarginInfo(false, false, true, false));
+        
+        
+        
+        HideOnClickLayout comparisonLevelLayout = new HideOnClickLayout("Datasets", heatmapFilter, null);
+        this.addComponent(comparisonLevelLayout);     
+        comparisonLevelLayout.setVisability(true);
+        
+        proteinsLayout = new ProteinsLayout(exploringFiltersManager);
+        HideOnClickLayout proteinsLevelLayout = new HideOnClickLayout("Proteins", proteinsLayout, null){
+
+            @Override
+            public void layoutClick(LayoutEvents.LayoutClickEvent event) {
+                super.layoutClick(event); //To change body of generated methods, choose Tools | Templates.
+                proteinsLayout.redrawCharts();
+            }        
+        
+        };
+        this.addComponent(proteinsLevelLayout);
+        proteinsLevelLayout.setVisability(true);
+        
+//         DatasetExploringHM datasestExploringHM = new DatasetExploringHM(exploringFiltersManager);
+//        this.addComponent(datasestExploringHM);
+        
+//        PublicationExplorerTreeLayout publicationTreeLayout = new PublicationExplorerTreeLayout(handler.getQuantDatasetListObject().getQuantDatasetList(),exploringFiltersManager);
+//        comparisonLayout.addComponent(publicationTreeLayout);
+
+//        //init level 1 heatmapFiltere    
+//        filtersLayout = filterUtility.initPopupFiltersLayout(exploringFiltersManager);
+//
+//        MiniFilterLayout minLayout = new MiniFilterLayout(exploringFiltersManager);
+//        HideOnClickLayout mainExploringFiltersLayout = new HideOnClickLayout("Datasets Exploring", filtersLayout, minLayout);
+//        this.addComponent(mainExploringFiltersLayout);
+//        mainExploringFiltersLayout.setVisability(true);
+//        //level 2 table layout
+//
+////level 3 study overall chart
+//        studiesExplorerTreeLayout = new DatasetsExplorerTreeLayout(exploringFiltersManager);
+//        //init level 2 heatmapFiltere
+//        this.addComponent(studiesExplorerTreeLayout);
+//
+        ExploreDatasetsTableLayout studiesTable = new ExploreDatasetsTableLayout(exploringFiltersManager,filterUtility.getActiveHeaders());
+        this.addComponent(studiesTable); 
+
+
+    }
+    
+    public void redrawCharts(){
+    proteinsLayout.redrawCharts();
+    compTableLayout.redrawCharts();
+    }
+    
+    
+    
+    
+
+    
+
+    
+}
